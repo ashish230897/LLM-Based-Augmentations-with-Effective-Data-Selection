caac74b3a9cc4b874e0d12952a115df2bc68d25c
Ashish Agrawal <ashish.agrawal2123@gmail.com>
2023-09-14 21:37:10 +0530

    Adding files to tune llama2 on target data of bnsentiment task

--- train_llama2/tune_llama2.py
+++ train_llama2/tune_llama2.py
@@ -2,8 +2,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from datasets import Dataset,DatasetDict
 import torch
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
-import transformers
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer, HfArgumentParser
 from peft import LoraConfig
 from transformers import TrainingArguments
 from trl import SFTTrainer
@@ -11,17 +10,20 @@ import random
 import numpy as np
 import os
 import time
-import argparse
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 os.environ["CUBLAS_WORKSPACE_CONFIG"]=":4096:8"
 os.environ["WANDB_DISABLED"] = "true"
 
-repo_path = "/raid/speech/ashish/TSTG_new"
+repo_path = os.getcwd()
 
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 torch.use_deterministic_algorithms(True, warn_only=True)
+
+model_args, data_args, training_args = None, None, None
 
 def set_seed(seed):
     random.seed(seed)
@@ -29,108 +31,63 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-model_name = "meta-llama/Llama-2-7b-hf"
+@dataclass
+class ModelArguments:
+    # Base model parameters
+    model_name_or_path: Optional[str] = field(default="bigscience/mt0-large")
     
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-
-model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        quantization_config=bnb_config,
-        trust_remote_code=True,
-        use_auth_token="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",
-        device_map="auto"
-    )
-model.config.use_cache = False
-
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, use_auth_token="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")
-tokenizer.pad_token = tokenizer.eos_token
-
-
-# taking lora config from huggingface repo: https://colab.research.google.com/drive/1VoYNfYDKcKRQRor98Zbf2-9VQTtGJ24k?usp=sharing#scrollTo=Ybeyl20n3dYH
-lora_alpha = 32
-lora_dropout = 0.05
-lora_r = 8
-
-peft_config = LoraConfig(
-    lora_alpha=lora_alpha,
-    lora_dropout=lora_dropout,
-    r=lora_r,
-    bias="none",
-    task_type="CAUSAL_LM",
-    target_modules=["q_proj", "k_proj", "v_proj"]
-)
-
-output_dir = None
-per_device_train_batch_size = 16
-gradient_accumulation_steps = 4  # making the effective batch size as 64
-optim = "paged_adamw_8bit"  # following hf repo hyper params
-save_steps = 20
-eval_steps = 20
-logging_steps = 1
-learning_rate = 2e-4
-max_grad_norm = 0.3
-max_steps = 120
-warmup_ratio = 0.03
-lr_scheduler_type = "constant"
-num_train_epochs = 10
-
-max_seq_length = 512
-
-def generate_dummy():
-
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-        device_map="auto",
+    load_in_8bit: bool = field(
+                default=False, metadata={"help": "Whether to convert the loaded model into mixed-8bit quantized model."}
     )
 
-    sequences = pipeline(
-        ["<s>"],
-        max_length=100,
-        do_sample=True,
-        top_k=20,
-        top_p=0.95,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        early_stopping=True
-    )
-    for seq in sequences:
-        print(f"Result: {seq[0]['generated_text']}")
+    device_map: Optional[str] = field(default="auto")
+    
+    # LoRA parameters
+    bnb_4bit_quant_type: str = field(default="nf4")
+    lora_r: int = field(default=64, metadata={"help": "Lora rank."})
+    lora_alpha: int = field(default=32, metadata={"help": "Lora alpha."})
+    lora_dropout: float = field(default=0.05, metadata={"help": "Lora dropout."})
+
+
+@dataclass
+class DataArguments:
+    
+    file_path: Optional[str] = field(default='data/en/wiki_auto/train_mt0.csv', metadata={"help": "Path to the training file."})
+    max_seq_length: Optional[int] = field(
+        default=256)
 
 
 def train_model(train_df, test_df):
 
-    training_arguments = TrainingArguments(
-        output_dir=output_dir,
-        per_device_train_batch_size=per_device_train_batch_size,
-        gradient_accumulation_steps=gradient_accumulation_steps,
-        optim=optim,
-        save_steps=save_steps,
-        save_total_limit=1,
-        load_best_model_at_end=True,
-        logging_steps=logging_steps,
-        learning_rate=learning_rate,
-        fp16=True,
-        # max_steps=max_steps,
-        max_grad_norm=max_grad_norm,
-        num_train_epochs=num_train_epochs,
-        evaluation_strategy="steps",
-        eval_steps=eval_steps,
-        metric_for_best_model="eval_loss",
-        warmup_ratio=warmup_ratio,
-        group_by_length=True,
-        lr_scheduler_type=lr_scheduler_type,
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type=model_args.bnb_4bit_quant_type,
+        bnb_4bit_compute_dtype=torch.bfloat16
     )
 
-    print("output dir is {}".format(output_dir))
+    model = AutoModelForCausalLM.from_pretrained(
+            model_args.model_name_or_path,
+            quantization_config=bnb_config,
+            trust_remote_code=True,
+            use_auth_token="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",
+            device_map=model_args.device_map
+        )
+    model.config.use_cache = False
+
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, trust_remote_code=True, use_auth_token="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")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    peft_config = LoraConfig(
+        lora_alpha=model_args.lora_alpha,
+        lora_dropout=model_args.lora_dropout,
+        r=model_args.lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=["q_proj", "k_proj", "v_proj"]
+    )
+
+    print("output dir is {}".format(training_args.output_dir))
     print("Number of train examples are {}".format(len(train_df)))
 
     dataset_dict = DatasetDict({
@@ -139,21 +96,18 @@ def train_model(train_df, test_df):
     })
 
     print("Pad token is {}".format(tokenizer.pad_token))
+    print("EOS token is {}".format(tokenizer.pad_token))
     print()
-    print("Dummy generation before fine-tuning----------")
-    t1 = time.time()
-    generate_dummy()
-    print("----------------- Time taken is {}".format(time.time()-t1))
-    
+
     trainer = SFTTrainer(
         model=model,
         train_dataset=dataset_dict['train'],
         eval_dataset=dataset_dict['test'],
         peft_config=peft_config,
         dataset_text_field="text_label",
-        max_seq_length=max_seq_length,
+        max_seq_length=data_args.max_seq_length,
         tokenizer=tokenizer,
-        args=training_arguments,
+        args=training_args,
     )
 
     for name, module in trainer.model.named_modules():
@@ -163,39 +117,25 @@ def train_model(train_df, test_df):
     
     # start training
     trainer.train()
-    trainer.save_model(os.path.join(output_dir,'final_model'))
+    trainer.save_model(os.path.join(training_args.output_dir,'final_model'))
 
 
 def main():
 
-    global output_dir
-
-    parser = argparse.ArgumentParser()
-
-    # Required parameters
-    parser.add_argument(
-        "--seed",
-        type=int,
-        required=True
-    )
-
-    parser.add_argument(
-        "--size",
-        type=int,
-        required=True
-    )
-
-    args = parser.parse_args()
+    global data_args
+    global model_args
+    global training_args
 
     set_seed(42)
 
-    output_dir = "{}/results/llama2/xnli_seed{}_size{}_label".format(repo_path, args.seed, args.size)
+    parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
     
     # create output dir if it does not exists
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not os.path.exists(training_args.output_dir):
+        os.makedirs(training_args.output_dir)
 
-    df = pd.read_csv("{}/data/xnli/train-ensubsetseed{}size{}_texts.csv".format(repo_path, args.seed, args.size))
+    df = pd.read_csv(data_args.file_path)
 
     train_df, test_df = train_test_split(df, test_size=0.1, random_state=42)
 
---
+++ train_llama2/tune_llama2.sh
@@ -0,0 +1,63 @@
+curr_dir=${PWD}
+
+# model args, taking lora config from huggingface repo: https://colab.research.google.com/drive/1VoYNfYDKcKRQRor98Zbf2-9VQTtGJ24k?usp=sharing#scrollTo=Ybeyl20n3dYH
+model_name="meta-llama/Llama-2-7b-hf"
+device_map="auto"
+lora_alpha=32
+lora_dropout=0.05
+lora_r=8
+bnb_4bit_quant_type="nf4"
+
+# data args
+max_seq_length=512
+file_path="${curr_dir}/data/bnsentiment/train-en_texts.csv"
+
+# training args
+per_device_train_batch_size=16
+gradient_accumulation_steps=4
+optim="paged_adamw_8bit"  # following hf repo hyper params
+save_steps=20
+eval_steps=20
+logging_steps=1
+learning_rate=2e-4
+max_grad_norm=0.3
+warmup_ratio=0.03
+lr_scheduler_type="constant"
+num_train_epochs=10
+output_dir="${curr_dir}/results/llama2/bnsentiment_entraineostokenfp16/"
+save_total_limit=1
+evaluation_strategy="steps"
+metric_for_best_model="eval_loss"
+
+# --group_by_length \
+# --optim $optim \
+# --fp16 \
+
+python ${curr_dir}/train_llama2/tune_llama2.py \
+    --model_name_or_path $model_name \
+    --bnb_4bit_quant_type $bnb_4bit_quant_type \
+    --device_map $device_map \
+    --lora_alpha $lora_alpha \
+    --lora_dropout $lora_dropout \
+    --lora_r $lora_r \
+    --output_dir $output_dir \
+    --per_device_train_batch_size $per_device_train_batch_size \
+    --gradient_accumulation_steps $gradient_accumulation_steps \
+    --optim $optim \
+    --save_steps $save_steps \
+    --eval_steps $eval_steps \
+    --logging_steps $logging_steps \
+    --learning_rate $learning_rate \
+    --max_grad_norm $max_grad_norm \
+    --warmup_ratio $warmup_ratio \
+    --lr_scheduler_type $lr_scheduler_type \
+    --max_seq_length $max_seq_length \
+    --num_train_epochs $num_train_epochs \
+    --load_best_model_at_end \
+    --save_total_limit $save_total_limit \
+    --evaluation_strategy $evaluation_strategy \
+    --metric_for_best_model $metric_for_best_model \
+    --group_by_length \
+    --file_path $file_path \
+    --fp16
+    
