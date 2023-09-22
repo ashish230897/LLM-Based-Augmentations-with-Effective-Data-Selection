4d78c23311d61b7b3fb258bf9e9784220296cada
Ashish Agrawal <ashish.agrawal2123@gmail.com>
2023-09-22 14:44:25 +0530

    create pretrain data

--- utils/createPretrainData.py
+++ utils/createPretrainData.py
@@ -1,20 +1,71 @@
 import os
 import pandas as pd
+import argparse
 
 repo_path = os.getcwd()
 
-def form_data():
-    data_path = repo_path + "/data/xnli/pretraining/processed_mr.csv"
+def form_data(seed, size, lang):
+    data_path = repo_path + "/data/xnli/pretraining/processed_{}seed{}size{}.csv".format(lang, seed, size)
     df = pd.read_csv(data_path)
     premises, hypos = list(df["Premises"]), list(df["Hypothesis"])
 
-    file = open(repo_path + "/data/xnli/pretraining/pretrain_mr.txt", "w+")
+    file = open(repo_path + "/data/xnli/pretraining/pretrain_{}_seed{}size{}.txt".format(lang, seed, size), "w+")
     for i,pre in enumerate(premises):
         file.write(pre + " " + hypos[i] + "\n")
     file.close()
 
+def form_data_bnsentiment(path):
+    
+    df = pd.read_csv(path)
+    texts = list(df["Texts"])
+
+    file = open(path + ".pretrain", "w+")
+    for text in texts:
+        file.write(text.strip().replace("\n", "") + "\n")
+    file.close()
+
+
 def main():
-    form_data()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--task",
+        type=str,
+        required=True
+    )
+
+    parser.add_argument(
+        "--path",
+        type=str,
+        required=False
+    )
+    
+    parser.add_argument(
+        "--seed",
+        type=int,
+        required=False
+    )
+
+    parser.add_argument(
+        "--size",
+        type=int,
+        required=False
+    )
+
+    parser.add_argument(
+        "--lang",
+        type=str,
+        required=False
+    )
+
+    args = parser.parse_args()
+
+    if args.task == "xnli":
+        form_data(args.seed, args.size, args.lang)
+    else:
+        form_data_bnsentiment(args.path)
+
 
 
 
