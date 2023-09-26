1b775b70f447e812d59e69edb135e80261d97649
Ashish Agrawal <ashish.agrawal2123@gmail.com>
2023-09-26 19:54:33 +0530

    add post processing of generations for xnli

--- train_llama2/post_process.py
+++ train_llama2/post_process.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import os
+import argparse
 
 path = os.getcwd()
 
-def main():
-    df = pd.read_csv("/home/ashish/taskspecificgenerations/results/generations_label_sst5chat_7bquantized.csv")
+def sst5_postprocess(args):
+
+    df = pd.read_csv(args.input_path)
     texts = list(df["Texts"])
 
     new_texts = []
@@ -20,7 +22,54 @@ def main():
     
     dict = {"Texts": new_texts}
     df_new = pd.DataFrame(dict)
-    df_new.to_csv("{}/results/generations_label_sst5chat_7bquantized_processed.csv".format(path), index=False)
+    df_new.to_csv(args.output_path, index=False)
+
+
+def xnli_postprocess(args):
+
+    df = pd.read_csv(args.input_path)
+    texts = list(df["Texts"])
+
+    hypos = []
+    for text in texts:
+        hypo_text = text.split('[/INST]')[1]
+
+        if ":" in hypo_text:
+            hypos.append(hypo_text.split(":")[1])
+        else:
+            hypos.append(hypo_text)
+    
+    df = pd.read_csv(args.premises_input_path)
+    premises = list(df["Texts"])
+    premises_triple = []
+    for pre in premises:
+        premises_triple += [pre]*3
+
+    print(len(premises_triple), len(hypos))
+
+    assert len(premises_triple) == len(hypos)
+
+    
+    dict = {"Premises": premises_triple, "Hypothesis": hypos}
+    df_new = pd.DataFrame(dict)
+    df_new.to_csv(args.output_path, index=False)
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", required=True, type=str)
+    parser.add_argument("--premises_input_path", required=False, type=str)
+    parser.add_argument("--output_path", required=True, type=str)
+    parser.add_argument("--task", required=True, type=str)
+    args = parser.parse_args()
+
+
+    if args.task == "sst5":
+        sst5_postprocess(args)
+    elif args.task == "xnli":
+        xnli_postprocess(args)
+
 
 
 
