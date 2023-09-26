3a27f56e372d06a89701fe834f1c4aa1d87285e9
Ashish Agrawal <ashish.agrawal2123@gmail.com>
2023-09-26 19:51:30 +0530

    added cleaning for xnli data

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -41,7 +41,7 @@ def main(input_path, output_path):
     texts = [re.sub(pattern, r'\1', text) for text in texts]  # remove 2. etc at the end of strings
 
     # pick texts having length > 4
-    texts = [text for text in texts if len(text) > 4]
+    texts = [text for text in texts if len(text) > 17]
 
     texts = list(set(texts))
 
@@ -53,6 +53,65 @@ def main(input_path, output_path):
     
     new_df = pd.DataFrame({"Texts": texts})
     new_df.to_csv(output_path, index=False)
+
+
+def process_xnli(args):
+
+    df = pd.read_csv(args.input_path)
+    premises = list(df["Premises"])
+    hypos = list(df["Hypothesis"])
+
+    print(premises[0])
+    print(hypos[0])
+
+    punctuation_chars = re.escape(string.punctuation)
+    pattern_1 = r'\s*([' + punctuation_chars + r'])\s*'
+    pattern_2 = r'([\d])([' + punctuation_chars + r'])\s*([^0-9])'
+    pattern_3 = r'([\w])([?.!,%])\s*([\w\d])'
+    pattern_4 = r'([' + punctuation_chars + r'])\s+\d+\.'
+    pattern_5 = r'([^\w\s])\1+'
+
+
+    new_premises, new_hypos, new_labels = [], [], []
+    label_list = ["entailment", "neutral", "contradiction"]
+    
+    labels = []
+    for i,pre in enumerate(premises):
+        labels.append(label_list[int(i%3)])
+
+
+    for i,(pre,hyp) in enumerate(zip(premises, hypos)):
+        
+        if type(hyp) != str:
+            continue
+
+        hyp = ''.join([i if ord(i) < 128 else '' for i in hyp])
+        hyp = remove_whitespace(hyp)
+        hyp = re.sub(r'\d+\.\s+', '', hyp)        
+        
+        hyp = re.sub(pattern_1, r'\1', hyp)  # remove spaces between punctuations
+        hyp = re.sub(pattern_2, r'\1\2 \3', hyp)
+
+        hyp = re.sub(pattern_3, r'\1\2 \3', hyp)
+
+        hyp = re.sub(pattern_4, r'\1', hyp)  # remove 2. etc at the end of strings
+
+        # pick texts having length > 17
+        if len(hyp) <= 17:
+            continue
+
+        # replace continuous punctuations with a single punctuation
+        hyp = re.sub(pattern_5, r'\1', hyp)
+
+        new_premises.append(pre.strip().replace("\n", ""))
+        new_hypos.append(hyp.strip().replace("\n", ""))
+        
+        new_labels.append(labels[i])
+    
+    assert len(new_premises) == len(new_hypos) == len(new_labels)
+
+    new_df = pd.DataFrame({"Premises": new_premises, "Hypothesis": new_hypos, "Label": new_labels})
+    new_df.to_csv(args.output_path, index=False)
 
 
 def test(texts):
@@ -94,9 +153,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", required=False, type=str)
     parser.add_argument("--output_path", required=False, type=str)
+    parser.add_argument("--task", required=True, type=str)
 
     args = parser.parse_args()
     
 
     # test(["It is better to show the faces of the people. Rana, Aparna, where is Bhaiya. 3."])
-    main(args.input_path, args.output_path)
+    
+    if args.task == "xnli":
+        process_xnli(args)
+    else:
+        main(args.input_path, args.output_path)
