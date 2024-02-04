578cded8b6c6ec8a511a460424faf5c92a3c0fd6
Ashish Agrawal <ashish.agrawal2123@gmail.com>
2024-02-04 15:57:41 +0530

    script to get overlap between teacher and prompt labels

---
+++ utils/get_overlapdata.py
@@ -0,0 +1,32 @@
+file = open("./data/snli/zero-shot_topk/expt-2/hard/train-hi.tsv")
+lines = file.readlines()
+file.close()
+
+teacher_labels = ["neutral"]*2500 + ["entailment"]*2500 + ["contradiction"]*2500
+prompt_labels = []
+matched_texts, matched_labels = [], []
+
+for i in range(7500):
+    label = lines[i].split("\t")[2]
+    text = lines[i].split("\t")[0].strip() + "\t" + lines[i].split("\t")[1].strip()
+
+    if label == teacher_labels[i]:
+        matched_texts.append(text)
+        matched_labels.append(label)
+
+
+file = open("./data/snli/zero-shot_topk/expt-2/overlap_hard/train-hi.tsv", "w+")
+for i in range(len(matched_labels)):
+    file.write(matched_texts[i] + "\t" + matched_labels[i] + "\t" + "1\n")
+
+for i in range(7500, len(lines)):
+    file.write(lines[i])
+file.close()
+
+file = open("./data/snli/zero-shot_topk/expt-2/overlap_soft/train-hi.tsv", "w+")
+for i in range(len(matched_labels)):
+    file.write(matched_texts[i] + "\t" + matched_labels[i] + "\t" + "0\n")
+
+for i in range(7500, len(lines)):
+    file.write(lines[i])
+file.close()
